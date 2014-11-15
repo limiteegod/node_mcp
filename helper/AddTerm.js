e72bd8b531495bd627a7a698cf840605b51d8064
root <root@localhost.localdomain>
2014-11-15 15:27:52 +0800

    生成期次函数

---
+++ helper/AddTerm.js
@@ -0,0 +1,69 @@
+var async = require('async');
+var esut = require('easy_util');
+var constants = require('mcp_constants');
+var dc = require('mcp_db').dc;
+var log = esut.log;
+
+var moment = require("moment");
+
+var initTerm = function()
+{
+    async.waterfall([
+        function(cb){
+            dc.init(function(err){
+                cb(err);
+            });
+        },
+        function(cb)
+        {
+            var table = dc.main.get("term");
+
+            var startDate = 20141115;
+            var endDate = 20141129;
+            var rst = [];
+            for(var curDate = startDate; curDate <= endDate; curDate++)
+            {
+                var startTimeStamp = moment(curDate + "000000", "YYYYMMDDHHmmss").valueOf();
+                var gap = 60*60*1000;
+                for(var i = 1; i < 25; i++)
+                {
+                    var start = startTimeStamp + (i - 1)*gap;
+                    var end = startTimeStamp + i*gap;
+                    var code = (curDate*100 + i) + "";
+                    var nextCode = "";
+                    if(i == 24)
+                    {
+                        nextCode += ((curDate+1)*100 + 1) + "";
+                    }
+                    else
+                    {
+                        nextCode += (curDate*100 + i + 1) + "";
+                    }
+                    var term = {gameCode:"T01", code:code, nextCode:nextCode,
+                        openTime:start, closeTime:end,
+                        status:constants.termStatus.NOT_ON_SALE, wNum:""};
+                    term.id = term.gameCode + "_" + term.code;
+                    rst[rst.length] = term;
+                }
+            }
+            log.info(rst);
+
+            async.eachSeries(rst, function(term, callback) {
+                table.save(term, {}, function(err, data){
+                    callback(err);
+                });
+            }, function(err){
+                cb(null);
+            });
+        },
+        function(cb)
+        {
+            cb(null, "success");
+        }
+    ], function (err, result) {
+        log.info(err);
+        log.info("end...........");
+    });
+};
+
+initTerm();
