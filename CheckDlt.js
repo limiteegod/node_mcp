987195ac6982158a3829bdfa81b93bd11b98184c
LiMing <liming@localhost.localdomain>
2014-11-27 12:33:08 +0800

    显示用户账户状态

--- CheckDlt.js
+++ CheckDlt.js
@@ -2,16 +2,17 @@ var dlt = require("mcp_dlt").dlt;
 
 //var validate = require("mcp_dlt").validate;
 
-var gameGrades = [400000000, 200000000, 100000000, 20000, 1000, 500];
+var gameGrades = [400000000, 32624000, 1106500, 20000, 1000, 500];
 var gl = dlt.gl(gameGrades.length);
 gl.setBonus(gameGrades);
 
 var check = dlt.check();
-console.log("123333333");
-check.setDrawNum("01,02,03,04,05|04,05");
+check.setDrawNum("03,05,07,15,34|01,10");
 check.setGl(gl);
 
+var val = check.count0500({number:'05,07,11,15,34|01,10'});
 
+console.log(val);
 
 /*
 console.log(check.count0000({number:"01,02,03,04,05|04,05;01,02,04,05,07|02,04;01,03,06,08,09|04,07"}));
@@ -30,5 +31,5 @@ console.log(check.count0001({number:"01,02,03,04,05,06,07,08,09|04,05,07,08,11"}
 
 //console.log(check.count0001({number:"01,02,03,04,05,06,07|04,05"}));
 
-console.log(check.count0002({number:"01,02,03,04$05,06,07,08,09,10,11,12,13,14,15,16|04$02,03,05"}));
+//console.log(check.count0002({number:"01,02,03,04$05,06,07,08,09,10,11,12,13,14,15,16|04$02,03,05"}));
 
